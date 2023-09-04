8d974820552c2fc4c10e335b0e7f29cf230d1de2
and96437345 <136622037+and96437345@users.noreply.github.com>
2023-09-04 21:24:53 +0300

    Lesson

---
+++ script.js
@@ -0,0 +1,31 @@
+// var name = 0;
+// let name2 = 0;
+// const constName = 0;
+// console.log(constName)
+
+// const block = document.querySelector(".block")
+// console.log(block)
+// const insideBlock = block.querySelector(".insideblock")
+
+// alert(insideBlock)
+
+// let value = prompt("Введите число", " ")
+// alert(value **)
+
+// let first = prompt ("first number", "")
+// let second = prompt ("second number", "")
+// alert((+first + +second) / 2)
+
+// let a =0 
+// console.log(a)
+
+// document.querySelector(".block")
+$('.block').click(function() {
+    // $(".block").toggleClass("newClass")
+    // $(".block").addClass("newClass")
+    // $(".block").removeClass("newClass")
+    $(this).toggleClass("newClass")
+})
+
+
+
