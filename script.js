1cc7b2f48c489ea77043262fe13e8d3eb2f3dbc1
Andrey <136622037+and96437345@users.noreply.github.com>
2023-09-06 21:29:58 +0300

    Lesson jquery

--- script.js
+++ script.js
@@ -20,12 +20,63 @@
 // console.log(a)
 
 // document.querySelector(".block")
-$('.block').click(function() {
+// $('.block').click(function() 
     // $(".block").toggleClass("newClass")
     // $(".block").addClass("newClass")
     // $(".block").removeClass("newClass")
-    $(this).toggleClass("newClass")
+//     $(this).toggleClass("newClass")
+// })
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let block = document.querySelector(".block")
+
+// function nameFunction(x,y) {
+//     console.log(x + y)   
+// }
+// nameFunction() 
+
+
+
+
+// --- РЕКУРСИЯ ---------------
+
+// let value = +prompt("Enter number", '')
+
+// function factorial(value) {
+//     if (value === 1) {
+//         return value
+//     }
+//     return value*factorial(value - 1)
+// }
+
+// alert(factorial(value))
+
+// -----------------------------
+
+
+$(".child11").click(function(){
+    // $(this).toggleClass("isActive")
+    $(this).parent().next().toggleClass("isActive")
+    .parent() - выход на родителя
+    .next() - следующий в узле
+    .prev() - предыдущий в узле
+    .children() - выход на ребенка
+    .closest(".allParent") выход на allParent
+    .find(".child44") поиск элемента child44
+    .siblings() - все соседние элементы
 })
+
+
+
+
 
 
 
