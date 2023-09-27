cc5123a6facfcafc744efb29d5917ae123668e69
Andrey <136622037+and96437345@users.noreply.github.com>
2023-09-27 21:16:37 +0300

    Lesson

--- script.js
+++ script.js
@@ -439,25 +439,102 @@
 // }
 // }
 
-let value = Math.floor(Math.random() * (100 - 1 + 1) +1)
+// let value = Math.floor(Math.random() * (100 - 1 + 1) +1)
 
 
-function game(){
-    let answer = +prompt('Угадайте число от 1 до 100');
-    if (answer > value) {
-        alert('Меньше');
-        game();
-        return;
-    } else if (answer < value) {
-        alert('Больше');
-        game();
-        return;
-    } else if (answer == value) {
-        alert('Угадал')
-    }
-}
+// function game(){
+//     let answer = +prompt('Угадайте число от 1 до 100');
+//     if (answer > value) {
+//         alert('Меньше');
+//         game();
+//         return;
+//     } else if (answer < value) {
+//         alert('Больше');
+//         game();
+//         return;
+//     } else if (answer == value) {
+//         alert('Угадал')
+//     }
+// }
 
-game()
+// game()
 
 // проверки на больше 100 меньше 0
 // еще раз запросить, чтобы сыграли
+
+// const btn = document.querySelector("#button")
+// console.log(btn)
+
+// const btn1 = $("#button")
+// console.log(btn1[0])
+
+// btn.addEventListener("click", function(){
+//     console.log("Что будем делать с этим элементом")
+// })
+
+// $("#button").hover(function(){
+//     console.log("eto hover")
+// })
+
+// btn.addEventListener("mouseover", function(){
+//     btn.style.color = "blue";
+// })
+
+// btn.addEventListener("mouseout", function(){
+//     btn.style.color = "red";
+// })
+
+// document.addEventListener("keyup", function(event){
+//   console.log("Я нажал")
+//     alert(event.key)//     if (event.key == "Escape") {
+// //         alert('Вы нажали Escape')
+// //     }
+//     if (event.key == "F5") {
+//         event.preventDefault()
+//         return
+//     }
+// })
+
+// const btn = document.querySelector(".button")
+// console.log(btn)
+
+// const clickOnBtn = function(event){
+//     console.log(event)
+//     console.log(event.screenX)
+//     btn.classList.add("newClass, newClass2")
+//     btn.classList.remove("newClass")
+//     btn.classList.toggle("newClass")
+//     if (btn.classList.contains("newClass")){
+
+//     }
+//     btn.classList.replace("oldClass", "newClass")
+// }
+
+
+// btn.addEventListener('click', clickOnBtn);
+// btn.removeEventListener('click', clickOnBtn);
+
+const tabHead = document.querySelector(".tabs__head")
+const tabBody = document.querySelector(".tabs__body")
+
+    // tabHead.addEventListener('click', function() {
+    // tabBody.classList.toggle(".tabs__body--open")
+    // })
+
+    // tabHead.addEventListener('click', () => {
+    //     tabBody.classList.toggle(".tabs__body--open")
+    // }
+
+    // $(".tabs__head").click(function()) {
+    //     $(".tabs__head").toggleClass("tabs__body--open")
+    // }
+
+    $(".tabs__caption").click(function() {
+
+    
+    $(this).addClass("tabs__caption--active");
+    $(this).siblings().removeClass("tabs__caption--active");
+    let currentTabIndex = $(this).index();
+    console.log(currentTabIndex);
+
+    })
